359b7b703bf8595ced683c3003feabe6c6bc2aa9
Farshid <farshidnooshi726@aut.ac.ir>
2021-02-01 16:22:01 +0330

    declaration comment added

--- Final Project-Routing with Graph Algorithms/Analyser.cpp
+++ Final Project-Routing with Graph Algorithms/Analyser.cpp
@@ -8,6 +8,8 @@ using namespace std;
 typedef long long ll;
 
 void Analyser::solve(queue<Query> q) {
+	//first := time of request. second->first := edge of request, second->second := value of request.
 	priority_queue<pair<ll, pair<Edge, int>>, vector<pair<ll, pair<Edge, int>>>, less<pair<ll, pair<Edge, int>>>> pq;
+
 
 }
