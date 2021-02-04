e2ce26e8a2679a5789ca37a27e83f6b02d56a13a
Farshid <farshidnooshi726@aut.ac.ir>
2021-02-04 19:15:35 +0330

    reference of auto iterator updated

--- Final Project-Routing with Graph Algorithms/Writer.cpp
+++ Final Project-Routing with Graph Algorithms/Writer.cpp
@@ -6,7 +6,7 @@ void Writer::WriteAnswers() {
 	for (auto& item : analysedMap) {
 		printf("Path from %d to %d will take %lf. \n", item.first.src, item.first.dst, item.second.timeElapsed);
 		cout << "points in the path from source to target :=\n";
-		for (auto u : item.second.path)
+		for (auto& u : item.second.path)
 			cout << u.src.id << ' ';
 		cout << item.first.dst << '\n';
 		cout << "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n";
