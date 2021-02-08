9d494c7d8766a290834b8a2b89e0bec826342b41
Farshid <farshidnooshi726@aut.ac.ir>
2021-02-09 00:07:50 +0330

    code update

--- Final Project-Routing with Graph Algorithms/Implementations/Analyser.cpp
+++ Final Project-Routing with Graph Algorithms/Implementations/Analyser.cpp
@@ -12,13 +12,13 @@ typedef long long ll;
 void Analyser::solve() {
 	//first := time of request. second->first := edge of request(src, dst), second->second := value of request.
 	auto compare = [](pair<double, pair<Edge, int>> lhs, pair<double, pair<Edge, int>> rhs) {
-		return lhs.first < rhs.first;
+		return rhs.first < lhs.first;
 	};
 	priority_queue<pair<double, pair<Edge, int>>, vector<pair<double, pair<Edge, int>>>, decltype(compare)> pq;
 
 	while (!queries.empty()) {
 		auto &item = queries.front();
-		if (!pq.empty() && (-pq.top().first) < item.tme) {
+		if (!pq.empty() && (pq.top().first) < item.tme) {
 			auto &it = pq.top();
 			dijkstra.UpdateGraph(it.second.first.src, it.second.first.dst, it.second.second);
 			pq.pop();
